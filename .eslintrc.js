ee04571b9142e394ecb82de3d5dddcc17723478f
Ryan <ryan@mysocialhustle.com>
2022-07-29 09:46:52 -0400

    Configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    amd: true,
+  },
+  rules: {
+    "no-console": "off",
+    "no-var-requires": "off",
+  },
+}
